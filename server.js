7f2dddde806c63f10825c18e902c60925a34b153
Jacob Kriese <jkriese12@gmail.com>
2021-03-22 06:46:27 -0700

    Change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(logger("dev"));
