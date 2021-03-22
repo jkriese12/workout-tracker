719ebdcfe3860513b837480b9e57faa4f0fce819
Jacob Kriese <jkriese12@gmail.com>
2021-03-22 06:42:26 -0700

    Updated server file with atlas route for heroku deployment

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/working-out", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
